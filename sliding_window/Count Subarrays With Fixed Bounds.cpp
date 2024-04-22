fd00a46321489aee1f4dbe7187c78659988cbf6f
rampal <rampalyadav23082001@gmail.com>
2024-04-22 17:15:49 +0530

    Graph_question_added

---
+++ sliding_window/Count Subarrays With Fixed Bounds.cpp
@@ -0,0 +1,66 @@
+Count Subarrays With Fixed Bounds
+
+link::-> https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/?envType=daily-question&envId=2024-03-31
+yt LInk::-->https://youtu.be/wQ0zRPv3ArU?si=OOYlW_P6D1nZaQRH
+
+You are given an integer array nums and two integers minK and maxK.
+
+A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
+
+The minimum value in the subarray is equal to minK.
+The maximum value in the subarray is equal to maxK.
+Return the number of fixed-bound subarrays.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+Output: 2
+Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+Example 2:
+
+Input: nums = [1,1,1,1], minK = 1, maxK = 1
+Output: 10
+Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+1 <= nums[i], minK, maxK <= 106
+
+
+
+solution::-->
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long res = 0;
+        bool minFound = false, maxFound = false;
+        int start = 0, minStart = 0, maxStart = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            if (num < minK || num > maxK) {
+                minFound = false;
+                maxFound = false;
+                start = i+1;
+            }
+            if (num == minK) {
+                minFound = true;
+                minStart = i;
+            }
+            if (num == maxK) {
+                maxFound = true;
+                maxStart = i;
+            }
+            if (minFound && maxFound) {
+                res += (min(minStart, maxStart) - start + 1);
+            }
+        }
+        return res;
+    }
+};
